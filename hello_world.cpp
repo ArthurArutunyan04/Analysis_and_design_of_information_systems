a3ac81d8c7ec3ed5a31ebf6fb6b77ef5b38c57a1
ArthurArutunyan04 <larturl2004@yandex.ru>
2024-12-01 23:42:01 +0300

    use clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
 // Основная функция программы
-int main() {
-    // Запрос имени пользователя
-    std::string name;
-    std::cout << "Введите ваше имя: ";
-    std::cin >> name;
-    // Вывод приветствия
-    std::cout << "Hello world from @" << name << std::endl;
-    return 0;
+int
+main()
+{
+  // Запрос имени пользователя
+  std::string name;
+  std::cout << "Введите ваше имя: ";
+  std::cin >> name;
+  // Вывод приветствия
+  std::cout << "Hello world from @" << name << std::endl;
+  return 0;
 }
